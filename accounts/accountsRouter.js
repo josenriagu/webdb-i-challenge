94c91baf337ea32d83c2ec9eab61dae064d6a2e9
Josemaria <josemarianriagu25@gmail.com>
2019-07-15 21:49:31 +0800

    implement GET by id api implement validateId middleware

--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -7,10 +7,34 @@ const router = express.Router();
 router.get('/', async (req, res) => {
    try {
       const accounts = await Accounts.get();
-      res.status(200).json(accounts)
+      res.status(200).json(accounts);
    } catch (error) {
       res.status(500).json('Oops! The aliens crushed the cables! Help us fix the mess from your side, while we work from over here')
    }
 })
+
+router.get('/:id', validateId, (req, res) => {
+   try {
+      res.status(200).json(req.account);
+   } catch (error) {
+      res.status(500).json('Oops! The aliens crushed the cables! Help us fix the mess from your side, while we work from over here')
+   }
+})
+
+// middleware
+async function validateId(req, res, next) {
+   const id = Number(req.params.id);
+   if (Number.isInteger(id)) {
+      const account = await Accounts.getById(id);
+      if (account.length !== 0) {
+         req.account = account;
+         next();
+      } else {
+         res.status(400).json("Aww, the account with that id has gone to the BlackHole")
+      }
+   } else {
+      res.status(400).json("That id doesn't look legit!")
+   }
+}
 
 module.exports = router;

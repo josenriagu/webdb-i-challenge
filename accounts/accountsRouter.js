691b6381935f617af0603d1e0ffa0132ebed46e8
Josemaria <josemarianriagu25@gmail.com>
2019-07-15 22:10:16 +0800

    implement POST api implement validateAccount middleware

--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -21,6 +21,14 @@ router.get('/:id', validateId, (req, res) => {
    }
 })
 
+router.post('/', validateAccount, async (req, res) => {
+   try {
+      const newAccount = await Accounts.insert(req.body);
+      res.status(201).json(newAccount);
+   } catch (error) {
+      res.status(500).json('Oops! The aliens crushed the cables! Help us fix the mess from your side, while we work from over here')
+   }
+})
 // middleware
 async function validateId(req, res, next) {
    const id = Number(req.params.id);
@@ -34,6 +42,18 @@ async function validateId(req, res, next) {
       }
    } else {
       res.status(400).json("That id doesn't look legit!")
+   }
+}
+
+async function validateAccount(req, res, next) {
+   if (Object.keys(req.body).length !== 0 && req.body.constructor === Object) {
+      if (req.body.name && req.body.budget) {
+         next();
+      } else {
+         res.status(400).json({ message: "Oh no! the required name and budget fields gone missing" })
+      }
+   } else {
+      res.status(400).json({ message: "hmm! no account data for real?" })
    }
 }
 

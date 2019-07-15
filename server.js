f52e541a8d58534a45f9d1e817c6abb38fcd808b
Josemaria <josemarianriagu25@gmail.com>
2019-07-15 21:19:37 +0800

    set up accounts router

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+const accountsRouter = require('./accounts/accountsRouter');
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', accountsRouter)
 
 server.get('/', (req, res) => {
    res.json("Welcome aboard my Spaceship")
